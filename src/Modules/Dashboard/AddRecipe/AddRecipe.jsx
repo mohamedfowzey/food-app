0a0f850c9d41ecfa55db3eaef43ce623eb5c4d5f
MohamedFowzey <fowzeym@gmail.com>
2026-05-10 13:08:08 +0300

    fix recipe image preview

--- src/Modules/Dashboard/AddRecipe/AddRecipe.jsx
+++ src/Modules/Dashboard/AddRecipe/AddRecipe.jsx
@@ -5,7 +5,9 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { toast } from "react-toastify";
+import LoadingElement from "../../Shared/LoadingElement/LoadingElement";
 export default function AddRecipe() {
+  const [loading, setLoading] = useState(false);
   const [recipeToEdit, setReciprToEdit] = useState(null);
   const { state } = useLocation();
   const [fileImage, setFileImage] = useState(null);
@@ -88,6 +90,8 @@ export default function AddRecipe() {
   };
   const getRecipeById = async (id) => {
     try {
+      setLoading(true);
+
       const res = await API.get(`Recipe/${id}`);
       setReciprToEdit(res.data);
       setValue("name", res?.data?.name);
@@ -97,7 +101,8 @@ export default function AddRecipe() {
       setValue("description", res?.data?.description);
       setValue("description", res?.data?.description);
       const path = res?.data?.imagePath;
-      saveImage(path);
+      await saveImage(path);
+      setLoading(false);
     } catch (e) {
       toast.error(e.response.data.message);
     }
@@ -156,133 +161,145 @@ export default function AddRecipe() {
         </div>
       </div>
       <div className="container mt-5 pt-5">
-        <form onSubmit={handleSubmit(onsubmit)}>
-          <div className="mb-3">
-            <input
-              {...register("name", { required: "recipe name is required" })}
-              type="text"
-              className="form-control"
-              placeholder="recipe name"
-              defaultValue={recipeToEdit?.name}
-            />
-            {errors.name && (
-              <span className="text-danger">{errors.name.message}</span>
-            )}
+        {loading ? (
+          <div className="d-flex justify-content-center align-items-center">
+
+            <LoadingElement />
           </div>
-          <div className="mb-3">
-            <select
-              {...register("tagId", { required: "tag name is required" })}
-              type="text"
-              className="form-control"
-              placeholder="tag name"
-            >
-              <option value={recipeToEdit?.tag?.id || ""}>
-                {recipeToEdit?.tag?.name || "tage name"}
-              </option>
-              {tags.map((t) => (
-                <option key={t.id} value={t.id}>
-                  {t.name}
-                </option>
-              ))}
-            </select>
-            {errors.tagId && (
-              <span className="text-danger">{errors.tagId.message}</span>
-            )}
-          </div>
-          <div className="mb-3">
-            <input
-              {...register("price", { required: "price is required" })}
-              type="number"
-              className="form-control"
-              placeholder="price"
-              defaultValue={recipeToEdit?.price}
-            />
-            {errors.price && (
-              <span className="text-danger">{errors.price.message}</span>
-            )}
-          </div>
-          <div className="mb-3">
-            <select
-              {...register("categoriesIds", {
-                required: "category name is required",
-              })}
-              type="text"
-              className="form-control"
-              placeholder="category name"
-            >
-              <option value={recipeToEdit?.category[0]?.id || ""}>
-                {recipeToEdit?.category[0]?.name || "category name"}
-              </option>
-              {categories.map((c) => (
-                <option key={c.id} value={c.id}>
-                  {c.name}
-                </option>
-              ))}
-            </select>
-            {errors.categoriesIds && (
-              <span className="text-danger">
-                {errors.categoriesIds.message}
-              </span>
-            )}
-          </div>
-          <div className="mb-3">
-            <textarea
-              {...register("description", {
-                required: "description is required",
-              })}
-              className="form-control"
-              placeholder="description"
-              defaultValue={recipeToEdit?.description}
-            ></textarea>
-          </div>
-          <div className="mb-3">
-            <div className="custom-file-input">
-              {recipeToEdit && recipeToEdit.imagePath ? (
-                <div style={{ width: 100 }} className="position-relative">
-                  <img
-                    width={100}
-                    src={`${BASE_URL}/${recipeToEdit?.imagePath}`}
-                    alt="recipeimage"
-                  />
-                  <span className="position-absolute start-100 top-0 fs-5 cursor-pointer text-nowrap ">
-                    <i
-                      className="fa fa-close text-danger"
-                      onClick={() => {
-                        setValue("recipeImage", [undefined]);
-                        setReciprToEdit((p) => ({ ...p, imagePath: "" }));
-                      }}
-                    ></i>
-                  </span>
-                </div>
-              ) : fileImage ? (
-                <img  src={URL.createObjectURL(fileImage)} />
-              ) : (
-                <>
-                  <h3>
-                    <label htmlFor="file-input" className="cursor-pointer">
-                      <i className="fa fa-upload text-main"></i>
-                    </label>
-                  </h3>
-                  <p>drag & drop or choose an image to upload</p>
-                </>
+        ) : (
+          <form onSubmit={handleSubmit(onsubmit)}>
+            <div className="mb-3">
+              <input
+                {...register("name", { required: "recipe name is required" })}
+                type="text"
+                className="form-control"
+                placeholder="recipe name"
+                defaultValue={recipeToEdit?.name}
+              />
+              {errors.name && (
+                <span className="text-danger">{errors.name.message}</span>
               )}
             </div>
-            <input
-              {...register("recipeImage")}
-              type="file"
-              accept="image/*"
-              id="file-input"
-              className="d-none"
-              placeholder="recipeImage"
-              onChange={(e) => setFileImage(e.target.files[0])}
-            />
-          </div>
-          <div className="text-end">
-            <div className="d-inline-block">
-              <MainButton>save</MainButton>
+            <div className="mb-3">
+              <select
+                {...register("tagId", { required: "tag name is required" })}
+                type="text"
+                className="form-control"
+                placeholder="tag name"
+              >
+                <option value={recipeToEdit?.tag?.id || ""}>
+                  {recipeToEdit?.tag?.name || "tage name"}
+                </option>
+                {tags.map((t) => (
+                  <option key={t.id} value={t.id}>
+                    {t.name}
+                  </option>
+                ))}
+              </select>
+              {errors.tagId && (
+                <span className="text-danger">{errors.tagId.message}</span>
+              )}
             </div>
-          </div>
-        </form>
+            <div className="mb-3">
+              <input
+                {...register("price", { required: "price is required" })}
+                type="number"
+                className="form-control"
+                placeholder="price"
+                defaultValue={recipeToEdit?.price}
+              />
+              {errors.price && (
+                <span className="text-danger">{errors.price.message}</span>
+              )}
+            </div>
+            <div className="mb-3">
+              <select
+                {...register("categoriesIds", {
+                  required: "category name is required",
+                })}
+                type="text"
+                className="form-control"
+                placeholder="category name"
+              >
+                <option value={recipeToEdit?.category[0]?.id || ""}>
+                  {recipeToEdit?.category[0]?.name || "category name"}
+                </option>
+                {categories.map((c) => (
+                  <option key={c.id} value={c.id}>
+                    {c.name}
+                  </option>
+                ))}
+              </select>
+              {errors.categoriesIds && (
+                <span className="text-danger">
+                  {errors.categoriesIds.message}
+                </span>
+              )}
+            </div>
+            <div className="mb-3">
+              <textarea
+                {...register("description", {
+                  required: "description is required",
+                })}
+                className="form-control"
+                placeholder="description"
+                defaultValue={recipeToEdit?.description}
+              ></textarea>
+            </div>
+            <div className="mb-3">
+              <div className="custom-file-input">
+                {(recipeToEdit && recipeToEdit.imagePath) || fileImage ? (
+                  <div
+                    style={{ width: 100 }}
+                    className="position-relative mx-auto"
+                  >
+                    <img
+                      width={100}
+                      src={
+                        recipeToEdit?.imagePath
+                          ? `${BASE_URL}/${recipeToEdit?.imagePath}`
+                          : URL.createObjectURL(fileImage)
+                      }
+                      alt="recipeimage"
+                    />
+                    <span className="position-absolute start-100 top-0 fs-5 cursor-pointer text-nowrap ">
+                      <i
+                        className="fa fa-close text-danger"
+                        onClick={() => {
+                          setValue("recipeImage", [undefined]);
+                          setReciprToEdit((p) => ({ ...p, imagePath: "" }));
+                        }}
+                      ></i>
+                    </span>
+                  </div>
+                ) : (
+                  <>
+                    <h3>
+                      <label htmlFor="file-input" className="cursor-pointer">
+                        <i className="fa fa-upload text-main"></i>
+                      </label>
+                    </h3>
+                    <p>drag & drop or choose an image to upload</p>
+                  </>
+                )}
+              </div>
+              <input
+                {...register("recipeImage")}
+                type="file"
+                accept="image/*"
+                id="file-input"
+                className="d-none"
+                placeholder="recipeImage"
+                onChange={(e) => setFileImage(e.target.files[0])}
+              />
+            </div>
+            <div className="text-end">
+              <div className="d-inline-block">
+                <MainButton>save</MainButton>
+              </div>
+            </div>
+          </form>
+        )}
       </div>
     </>
   );
